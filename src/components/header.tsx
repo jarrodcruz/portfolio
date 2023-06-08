84454a3853064f4d72c9a5443ef268baacdb5ec1
jrodcodes <cruz.jarroddd@gmail.com>
2023-06-08 13:57:29 -0700

    fixed esl lint issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <section className="mb-8">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
-        <Link href="/">Jarrod Cruz's Portfolio</Link>
+        <Link href="/">Jarrod Cruz&apos;s Portfolio</Link>
         <MyLinks />
       </div>
     </section>
